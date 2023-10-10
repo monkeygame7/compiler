012f6cdd0790fa27104349a422ee073d07878ecd
Shayan Motevalli <shayanmotevalli@gmail.com>
2023-10-10 07:50:05 -0400

    Add tests for parser

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -211,3 +211,198 @@ fn binary_operator_priority(token: &SyntaxToken) -> usize {
         _ => 0,
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::fmt::Display;
+
+    use super::*;
+
+    #[derive(PartialEq, Eq, Debug, Clone)]
+    enum Matcher {
+        Bad,
+        Int(i32),
+        Bool(bool),
+        Ident(String),
+        Binary(BinaryOperatorKind),
+        Unary(UnaryOperatorKind),
+    }
+
+    impl Display for Matcher {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let s = match self {
+                Matcher::Int(i) => i.to_string(),
+                Matcher::Bool(b) => b.to_string(),
+                Matcher::Ident(s) => s.to_string(),
+                _ => "".to_string(),
+            };
+            f.write_str(&s)
+        }
+    }
+
+    struct AssertingIterator {
+        pub nodes: Vec<Matcher>,
+        current: usize,
+    }
+
+    impl AssertingIterator {
+        fn new(text: String) -> Self {
+            let mut nodes = vec![];
+            let mut stack = vec![];
+            let ast = Parser::parse(text);
+
+            assert_eq!(ast.diagnostics.messages.take().len(), 0);
+
+            stack.push(ast.root);
+
+            while let Some(next) = stack.pop() {
+                let matcher = match next.kind {
+                    AstNodeKind::BadNode => Matcher::Bad,
+                    AstNodeKind::IntegerLiteral(i) => Matcher::Int(i),
+                    AstNodeKind::BooleanLiteral(b) => Matcher::Bool(b),
+                    AstNodeKind::Identifier(s) => Matcher::Ident(s),
+                    AstNodeKind::BinaryExpression(l, op, r) => {
+                        stack.push(*r);
+                        stack.push(*l);
+                        Matcher::Binary(op)
+                    }
+                    AstNodeKind::UnaryExpression(op, expr) => {
+                        stack.push(*expr);
+                        Matcher::Unary(op)
+                    }
+                };
+                nodes.push(matcher);
+            }
+
+            AssertingIterator { nodes, current: 0 }
+        }
+
+        fn assert(&mut self, matcher: Matcher) {
+            assert!(
+                self.current < self.nodes.len(),
+                "Unable to match {:?}, ran out of tokens",
+                matcher
+            );
+            assert_eq!(self.nodes[self.current], matcher);
+            self.current += 1;
+        }
+
+        fn finish(&mut self) {
+            assert_eq!(self.current, self.nodes.len(), "Did not match all nodes");
+        }
+    }
+
+    #[test]
+    fn test_unary_operator_stops_at_binary() {
+        binary_operators()
+            .into_iter()
+            .flat_map(|bin| unary_operators().into_iter().map(move |una| (bin, una)))
+            .for_each(|(bin, una)| {
+                primary_matchers()
+                    .into_iter()
+                    .flat_map(move |id1| {
+                        primary_matchers()
+                            .into_iter()
+                            .map(move |id2| (id1.clone(), id2.clone()))
+                    })
+                    .for_each(|(id1, id2)| {
+                        let s = format!("{}{} {} {}", una, id1, bin, id2);
+                        let mut asserter = AssertingIterator::new(s);
+                        asserter.assert(Matcher::Binary(bin));
+                        asserter.assert(Matcher::Unary(una));
+                        asserter.assert(id1);
+                        asserter.assert(id2);
+
+                        asserter.finish();
+                    })
+            })
+    }
+
+    #[test]
+    fn test_binary_operator_consumes_unary() {
+        binary_operators()
+            .into_iter()
+            .flat_map(|bin| unary_operators().into_iter().map(move |una| (bin, una)))
+            .for_each(|(bin, una)| {
+                primary_matchers()
+                    .into_iter()
+                    .flat_map(move |id1| {
+                        primary_matchers()
+                            .into_iter()
+                            .map(move |id2| (id1.clone(), id2.clone()))
+                    })
+                    .for_each(|(id1, id2)| {
+                        let s = format!("{} {} {}{}", id1, bin, una, id2);
+                        let mut asserter = AssertingIterator::new(s);
+                        asserter.assert(Matcher::Binary(bin));
+                        asserter.assert(id1);
+                        asserter.assert(Matcher::Unary(una));
+                        asserter.assert(id2);
+
+                        asserter.finish();
+                    })
+            })
+    }
+
+    #[test]
+    fn test_full() {
+        let text = "a + 2 * (!true && false) | (test / -+3)".to_string();
+        let mut asserter = AssertingIterator::new(text);
+
+        asserter.assert(Matcher::Binary(BinaryOperatorKind::BitwiseOr));
+        asserter.assert(Matcher::Binary(BinaryOperatorKind::Add));
+        asserter.assert(Matcher::Ident("a".to_string()));
+        asserter.assert(Matcher::Binary(BinaryOperatorKind::Mulitply));
+        asserter.assert(Matcher::Int(2));
+        asserter.assert(Matcher::Binary(BinaryOperatorKind::LogicalAnd));
+        asserter.assert(Matcher::Unary(UnaryOperatorKind::LogicalNot));
+        asserter.assert(Matcher::Bool(true));
+        asserter.assert(Matcher::Bool(false));
+        asserter.assert(Matcher::Binary(BinaryOperatorKind::Divide));
+        asserter.assert(Matcher::Ident("test".to_string()));
+        asserter.assert(Matcher::Unary(UnaryOperatorKind::Negate));
+        asserter.assert(Matcher::Unary(UnaryOperatorKind::Identity));
+        asserter.assert(Matcher::Int(3));
+
+
+        asserter.finish();
+    }
+
+    fn binary_operators() -> Vec<BinaryOperatorKind> {
+        vec![
+            BinaryOperatorKind::Add,
+            BinaryOperatorKind::Subtract,
+            BinaryOperatorKind::Mulitply,
+            BinaryOperatorKind::Divide,
+            BinaryOperatorKind::LogicalAnd,
+            BinaryOperatorKind::LogicalOr,
+            BinaryOperatorKind::BitwiseAnd,
+            BinaryOperatorKind::BitwiseOr,
+            BinaryOperatorKind::Equals,
+            BinaryOperatorKind::NotEquals,
+            BinaryOperatorKind::LessThan,
+            BinaryOperatorKind::LessThanOrEquals,
+            BinaryOperatorKind::GreaterThan,
+            BinaryOperatorKind::GreaterThanOrEquals,
+        ]
+    }
+
+    fn unary_operators() -> Vec<UnaryOperatorKind> {
+        vec![
+            UnaryOperatorKind::Identity,
+            UnaryOperatorKind::Negate,
+            UnaryOperatorKind::LogicalNot,
+        ]
+    }
+
+    fn primary_matchers() -> Vec<Matcher> {
+        vec![
+            Matcher::Int(1),
+            Matcher::Int(2),
+            Matcher::Bool(false),
+            Matcher::Bool(true),
+            Matcher::Ident("a".to_string()),
+            Matcher::Ident("b".to_string()),
+        ]
+    }
+}
