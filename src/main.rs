5842b846288cb96460ca72d0685cbdbc6997ad71
Shayan Motevalli <shayanmotevalli@gmail.com>
2023-10-10 23:37:54 -0400

    Allow multiline input
    Still needs a little cleanup with the errors (time to add line numbers?)

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,33 @@ mod diagnostics;
 mod evaluator;
 
 fn main() -> io::Result<()> {
-    let mut show_tree = true;
+    let mut show_tree = false;
+    let mut buffer = String::new();
     loop {
-        let mut buffer = String::new();
-        print!("» ");
-        io::stdout().flush()?;
-        io::stdin().read_line(&mut buffer)?;
+        let mut line_buffer = String::new();
 
-        if buffer.trim() == "#exit" {
-            break;
-        } else if buffer.trim() == "#tree" {
-            show_tree = !show_tree;
-            let msg = if show_tree { "" } else { "Not " };
-            println!("{}showing tree", msg);
-            continue;
-        } else if buffer.trim() == "#clear" {
-            print!("\x1B[2J\x1B[1;1H");
-            continue;
+        if buffer.is_empty() {
+            print!("» ");
+        } else {
+            print!("  ")
         }
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut line_buffer)?;
+
+        if buffer.is_empty() {
+            if line_buffer.trim() == "#exit" {
+                break;
+            } else if line_buffer.trim() == "#tree" {
+                show_tree = !show_tree;
+                let msg = if show_tree { "" } else { "Not " };
+                println!("{}showing tree", msg);
+                continue;
+            } else if line_buffer.trim() == "#clear" {
+                print!("\x1B[2J\x1B[1;1H");
+                continue;
+            }
+        }
+        buffer += &line_buffer;
 
         let tree = Parser::parse(buffer.to_owned());
         if show_tree {
@@ -34,17 +43,22 @@ fn main() -> io::Result<()> {
         let evaluator = Evaluator::new(tree);
         let result = evaluator.evaluate();
         match result {
-            Ok(r) => println!("{}", r),
-            Err(errors) => {
+            Ok(r) => {
+                println!("{}", r);
+                buffer.clear()
+            }
+            Err(errors) if line_buffer.trim().is_empty() => {
                 for error in errors {
                     println!("{}", error.message);
                     let span = error.span;
-                    let pre = &buffer[..span.start];
+                    let pre = &buffer[..span.start].trim_start();
                     let highlight = &buffer[span.start..span.end].red();
-                    let post = &buffer[span.end..];
+                    let post = &buffer[span.end..].trim_end();
                     println!("{}{}{}", pre, highlight, post);
                 }
+                buffer.clear()
             }
+            _ => continue,
         }
     }
 
